f5aef8a709892a7acda386a5e7ae853255eb600c
Ezequiel Alas <cheke_5@hotmail.com>
2016-08-11 00:10:07 -0500

    ejercicio 2 semana 3 algoritmos

---
+++ ejercicio 2 semana 3 algoritmos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParoImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero");
+            int a = Convert.ToInt16(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine(a + " es par");
+            }
+            else
+            {
+                Console.WriteLine(a + " es impar");
+            }
+            Console.ReadLine();
+        }
+    }
+}
