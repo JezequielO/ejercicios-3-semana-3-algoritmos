f8c2f902a9a72e77e4be3d4a456529f045b4baa8
Ezequiel Alas <cheke_5@hotmail.com>
2016-08-11 00:11:11 -0500

    ejercicio 3 semana 3 algoritmos

---
+++ ejercicio 3 semana 3 algoritmos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_3_semana_3_algoritmos
+{
+    class Program
+    {
+        class positive_negativo
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Ingrese un numero");
+                int a = Convert.ToInt16(Console.ReadLine());
+                if (a >= 0)
+                {
+                    Console.WriteLine(a + " es positivo");
+                }
+                else
+                {
+                    Console.WriteLine(a + " es negativo");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
