20c48de26ef1942d9df15a8a488c42946db3b100
Ezequiel Alas <cheke_5@hotmail.com>
2016-08-11 00:15:35 -0500

    ejercicio 6 semana 3 algoritmos

---
+++ ejercicios 6 semana 3 algoritmos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESTATURA
+{ class Program
+{ static void Main(string[] args)
+{ 
+    int num; 
+    string linea; 
+    Console.Write("INGRESE SU ALTURA EN CENTIMETROS:"); 
+    linea = Console.ReadLine();
+    num = int.Parse(linea); 
+    if (num < 150) 
+    { 
+        Console.Write("ES DE ALTURA BAJA");
+    }
+    else
+    {
+        Console.Write("ES DE ALTURA PROMEDIO"); 
+    }
+    if (num > 170) 
+    { 
+        Console.Write("ES DE UNA ALTURA MAYOR A LA PROMEDIO");
+    }
+    Console.ReadKey(); 
+}
+}
+}
